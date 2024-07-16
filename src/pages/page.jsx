03a60fe0a11899ea86c6db94bdf2c463cf0bce78
boburastonov <boburastonov33@gmail.com>
2024-07-16 11:15:33 +0500

    add support section to main page

--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./navbar/navbar";
 import Slider from "./slider/slider";
 import Tours from "./tours/tours";
+import VisaSupport from "./visasupport/support";
 
 const Page = () => {
   return (
@@ -9,6 +10,7 @@ const Page = () => {
       <Navbar />
       <Slider />
       <Tours />
+      <VisaSupport />
     </>
   );
 };
