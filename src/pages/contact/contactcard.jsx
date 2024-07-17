2afc461eb6630aa560c5de7fae23ecf9c1c31b1a
boburastonov <boburastonov33@gmail.com>
2024-07-17 14:43:36 +0500

    add box shadow to contact box

--- src/pages/contact/contactcard.jsx
+++ src/pages/contact/contactcard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contactcard = ({ icon, heading, linkUrl, linkText }) => {
   return (
-    <div className="w-[32%] bg-white rounded-[23px] shadow-[0px_0px_15px_rgba(0, 0, 0, 0.15)] text-center p-[30px]">
+    <div className="w-[32%] bg-white rounded-[23px] shadow-lg text-center p-[30px]">
       <div className="bg-[#f0f0f0] flex items-center justify-center mx-auto w-[60px] h-[60px] inline-block text-center leading-[60px] rounded-[50px] text-[#22b3c1] text-[20px] mb-5">
         {icon}
       </div>
