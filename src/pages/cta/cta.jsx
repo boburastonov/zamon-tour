5b4d35b788e7681bdccc6585015374bf2d58b7b7
boburastonov <boburastonov33@gmail.com>
2024-07-19 15:53:56 +0500

    Update texts

--- src/pages/cta/cta.jsx
+++ src/pages/cta/cta.jsx
@@ -9,7 +9,7 @@ const CTA = () => {
       <div className="max-w-7xl px-3 mx-auto flex items-center justify-between">
         <div className="w-[66%] pr-3 text-white">
           <h2 className="text-[40px] mb-[10px] uppercase font-bold">
-            {t('Are You Looking To Travel')}
+            {t('Are You Looking To Travel?')}
           </h2>
           <h4 className="text-[20px] font-semibold tracking-[0.5px] m-0">
             {t('Make A Reservation By Clicking The Button')}
