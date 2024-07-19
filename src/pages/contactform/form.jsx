99f38415e7af734cab4c5e6c1ea2a289625d7738
boburastonov <boburastonov33@gmail.com>
2024-07-19 15:52:33 +0500

    import axios

--- src/pages/contactform/form.jsx
+++ src/pages/contactform/form.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./form.css";
 import { useTranslation } from "react-i18next";
+import axios from "axios";
 
 const Form = () => {
   const { t } = useTranslation();
